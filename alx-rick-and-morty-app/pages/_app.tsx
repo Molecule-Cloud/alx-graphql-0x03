2663c165ffdb7f47f6082e24649a3a8dddf0ebc3
Benjamin Appiah <ben.appiah451@gmail.com>
2025-08-15 02:32:44 +0000

    Rick and morty app kickoff

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
-import client from "@/graphql/appolloClient";
+import client from "@/graphql/apolloClient";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
